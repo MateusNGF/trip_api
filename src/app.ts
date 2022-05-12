5c63a29b246b2c07ebd2f9e778d1fbd4b305425d
Gomess <mateus.nicolau@devmagic.com.br>
2022-05-11 21:26:15 -0300

    wip(*): create basic server and configuration orm

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import {userRoutes} from './users'
+
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+
+const app = express()
+
+app.use("/user", userRoutes)
+
+
+try{
+    createConnection()
+    app.listen(8080, () => {
+        console.log("Servidor ta bala.")
+    })
+
+}catch(erro){
+    console.log("error")
+}
+
+export default app;
