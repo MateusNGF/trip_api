37c000815865893fc28b10563340175c47b4081c
Daniel Borges <danprog91@gmail.com>
2022-05-14 23:37:56 -0300

    refactor: implemented duplicate comment blocking

--- src/features/comments/comments.repository.ts
+++ src/features/comments/comments.repository.ts
@@ -1,10 +1,31 @@
-import { EntityRepository, Repository } from "typeorm";
+import { on } from "events";
+import { EntityRepository, getCustomRepository, getRepository, IsNull, Repository } from "typeorm";
 import { Comments } from "./comments.entity";
 
 @EntityRepository(Comments)
 export class CommentsRepository extends Repository<Comments> {
+
   async createComment(comment: Comments) {
     const test = await comment.save()
     return test
+  }
+
+  async findCommentDuplicated(comment: string, touristSpot: string, user: string): Promise<Comments[]> {
+    const commentDuplicated = this.createQueryBuilder('c')
+      .where('comment = :comment', { comment })
+
+    if (touristSpot) {
+      commentDuplicated.andWhere('c.touristSpot = :touristSpot', { touristSpot })
+    }
+
+    if (user) {
+      commentDuplicated.andWhere('c.user = :user', { user })
+    }
+
+    const res = await commentDuplicated.getMany()
+
+    console.log(res)
+
+    return res;
   }
 }
--- src/features/comments/comments.routes.ts
+++ src/features/comments/comments.routes.ts
@@ -1,9 +1,28 @@
 import { Router } from "express"
+import { getCustomRepository } from "typeorm"
 import { Comments } from "./comments.entity"
+import { CommentsRepository } from "./comments.repository"
 
 export const commentsRoutes = Router()
 
 commentsRoutes.post("/create", async (req, res) => {
-  const newComment: Comments = new Comments(req.body)
-  return res.send(await newComment.save())
+  try {
+    const newComment: Comments = new Comments(req.body)
+
+    const repositoryComments = getCustomRepository(CommentsRepository)
+
+    const findDuplicatedComment = await repositoryComments.findCommentDuplicated(
+      newComment.comment,
+      newComment.touristSpot,
+      newComment.user
+    )
+
+    if (findDuplicatedComment.length > 0) {
+      throw new Error("Comment duplicated!")
+    }
+
+    return res.send(await newComment.save())
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
 })
